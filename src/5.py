b95aff87b96304e060ebd8f0909d672639f5eae2
thomjorrit812 <thomjorrit812@gmail.com>
2025-03-10 17:29:24 +0000

    Add 5.py

---
+++ src/5.py
@@ -0,0 +1,10 @@
+
+from math import *
+
+def solve_math_problem(problem):
+    if problem == 1:
+        return "Answer = 6"
+    elif problem == 2:
+        return "Answer = 42"
+    else:
+        return "Error: Unknown Problem"
