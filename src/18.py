9da4913cfe709773181da233186f1c449fb0af04
thomjorrit812 <thomjorrit812@gmail.com>
2025-03-23 23:00:18 +0000

    Add 18.py

---
+++ src/18.py
@@ -0,0 +1,12 @@
+def multiply_numbers(a: int, b: int) -> int:
+    """
+    Multiply two numbers.
+    
+    Parameters:
+        a (int): First number to be multiplied.
+        b (int): Second number to be multiplied.
+        
+    Returns:
+        int: The product of the two numbers.
+    """
+    return a * b
